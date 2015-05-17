707d18ce33cdb310aa19dd7db75b0fb516c3bad3
ShiNSanity75 <jj06280705@gmail.com>
2015-05-17 22:04:47 +0800

    main method 中的Scanner

--- RewiteLoan.java
+++ RewiteLoan.java
@@ -1,4 +1,25 @@
+import java.util.Scanner;
+
 public class RewiteLoan {
+
+	/** Main method */
+	public static void main(String[] args) {
+		// Create a Scanner
+		Scanner input = new Scanner(System.in);
+
+		// Enter yearly interest rate
+		System.out.print("Enter yearly interest rate, for example, 8.25: ");
+		double annualInterestRate = input.nextDouble();
+
+		// Enter number of years
+		System.out.print("Enter number of years as an integer: ");
+		int numberOfYears = input.nextInt();
+
+		// Enter loan amount
+		System.out.print("Enter loan amount, for example, 120000.95: ");
+		
+	}
+	
 	private double annualInterestRate;
 	private int numberOfYears;
 	private double loanAmount;
